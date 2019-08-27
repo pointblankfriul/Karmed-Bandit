e3eb20f1a73a84ddc5aa6c3e0b3876ea7ae909c0
EdoardoScian <54208794+EdoardoScian@users.noreply.github.com>
2019-08-27 16:49:07 +0200

    Add files via upload

--- Karmed.py
+++ Karmed.py
@@ -13,7 +13,7 @@ class Karmed(object):
         and the initial values of the rewards
         '''
         self.estimated_rewards = np.zeros(arms)
-        self.no_selections = np.zeros(arms)
+        self.act_selections = np.zeros(arms)
         self.leverages = []
         for i in range(arms):
             self.leverages.append(Leverage.Leverage(mus[i], sigmas[i]))
@@ -33,8 +33,25 @@ class Karmed(object):
         self.iter_values = np.zeros(iter_per_run)
 
 
+    def setRandomNoise(self, mu, sigma):
+        self.noise = Leverage.Leverage(mu, sigma)
+
+    
+    def _stationaryEstimates(self, iteration, action_index):
+        return 1/self.act_selections[action_index]*(self.iter_values[iteration] - self.estimated_rewards[action_index])
+
+    
+    def _nonStationaryEstimates(self, action_index, init_estimates):
+        exp_decay_sum = 0
+        i = 1
+        for reward in self.rewardsMat[action_index]:
+            exp_decay_sum += self.alpha*((1 - self.alpha)**(self.act_selections[action_index] - i))*reward
+            i += 1
+        return ((1-self.alpha)**self.act_selections[action_index])*init_estimates[action_index] + exp_decay_sum
+
+
     @abstractmethod
-    def train(self):
+    def run(self):
         raise NotImplementedError("Please Implement this method")
         
 
@@ -42,10 +59,13 @@ class KarmedStationary(Karmed):
 
     def __init__(self, arms, mus, sigmas, initial_reward_values, ucb_params):
         '''
-        Define the number of Leverages you want, their mus (numpy array), their sigmas (same)
-        and the initial values of the rewards
-        
-        ucb_params = (ucb_activation, c)
+        Class to manage stationary problems with a K-Armed Bandit
+
+        arms : int > 0 -- number of possible actions
+        mus : float numpy -- arms mean values
+        sigmas : float numpy -- arms sigma values
+        initial_reward_values : float -- first iteration estimates
+        ucb_params : tuple(ucb_activation, c)
                     - ucb_activation: bool --  Upper-Confidence Method. True to use
                     - c: float > 0 -- high values increase exploration
         '''
@@ -54,19 +74,23 @@ class KarmedStationary(Karmed):
         Karmed.__init__(self, arms, mus, sigmas, initial_reward_values)
 
 
-    def ucbSelection(self, estimates, t, act_selections):
+    def __ucbSelection(self, estimates, t, act_selections):
         ucb = []
-        for i in range(estimates):
-            ucb.append(estimates[i] + self.c*math.sqrt(math.log(t)/act_selections[i]))
+        i = 0
+        for estimate in estimates:
+            if act_selections[i] == 0:
+                return i
+            ucb.append(estimate + self.c*math.sqrt(math.log(t)/act_selections[i]))
+            i += 1
         return np.argmax(ucb)
     
     
-    def train(self):
+    def run(self):
         for iteration in range(self.iter_per_run):
             if (np.random.uniform(0, 1) <= self.epsilon):
                 # Exploring
                 if self.ucb_as == True:
-                    index = self.ucbSelection(self.estimated_rewards, iteration, self.no_selections)
+                    index = self.__ucbSelection(self.estimated_rewards, iteration, self.act_selections)
                 else:
                     index = np.random.randint(0, self.estimated_rewards.shape[0])
             else:
@@ -78,9 +102,9 @@ class KarmedStationary(Karmed):
                 else:
                     index = index[0]
             
-            self.no_selections[index] += 1
+            self.act_selections[index] += 1
             self.iter_values[iteration] = self.leverages[index].pull()
-            self.estimated_rewards[index] += 1/self.no_selections[index]*(self.iter_values[iteration] - self.estimated_rewards[index])
+            self.estimated_rewards[index] += self._stationaryEstimates(iteration, index)
         
         return self.iter_values, self.estimated_rewards
 
@@ -89,19 +113,22 @@ class KarmedNonStationary(Karmed):
 
     def __init__(self, arms, mus, sigmas, initial_reward_values, alpha):
         '''
-        Define the number of Leverages you want, their mus (numpy array), their sigmas (same),
-        the initial values of the rewards and the alpha decay parameter
+        Class to manage non-stationary problems with a K-Armed Bandit
+
+        arms : int > 0 -- number of possible actions
+        mus : float numpy -- arms mean values
+        sigmas : float numpy -- arms sigma values
+        initial_reward_values : float -- first iteration estimates
+        alpha : float > 0 -- step-size decay parameter
+
+        Always set gaussian noise with setRandomNoise!
         '''
         self.alpha = alpha
         self.rewardsMat = [[] for i in range(arms)]
         Karmed.__init__(self, arms, mus, sigmas, initial_reward_values)
 
 
-    def setRandomNoise(self, mu, sigma):
-        self.noise = Leverage.Leverage(mu, sigma)
-
-
-    def train(self):
+    def run(self):
         init_estimates = copy.deepcopy(self.estimated_rewards)
         for iteration in range(self.iter_per_run):
             if (np.random.uniform(0, 1) <= self.epsilon):
@@ -117,27 +144,41 @@ class KarmedNonStationary(Karmed):
                     index = index[0]
 
             # update number of leverage selection
-            self.no_selections[index] += 1
+            self.act_selections[index] += 1
             # get reward
             self.iter_values[iteration] = self.leverages[index].pull()
             # append reward for estimates update and update formula
-            self.rewardsMat[index].append(self.iter_values[iteration])
-            exp_decay_sum = 0
-            i = 1
-            for reward in self.rewardsMat[index]:
-                exp_decay_sum += self.alpha*((1 - self.alpha)**(self.no_selections[index] - i))*reward
-                i += 1
-                
-            self.estimated_rewards[index] = ((1-self.alpha)**self.no_selections[index])*init_estimates[index] + exp_decay_sum
+            self.rewardsMat[index].append(self.iter_values[iteration])               
+            self.estimated_rewards[index] = self._nonStationaryEstimates(index, init_estimates)
 
             # Add gaussian noise to all leverages
             for leverage in self.leverages:
-                orig_mu = leverage.getMu() 
+                orig_mu = leverage.getMu()
                 noise = self.noise.pull()
                 leverage.setMu(orig_mu+noise)
         
         return self.iter_values, self.estimated_rewards, self.leverages
 
+
+class KarmedGradient(Karmed):
+
+    def __init__(self, arms, mus, sigmas, initial_reward_values, alpha, stationarity):
+        '''
+        Class to manage gradient-ascent based K-Armed Bandit
+
+        arms : int > 0 -- number of possible actions
+        mus : float numpy -- arms mean values
+        sigmas : float numpy -- arms sigma values
+        initial_reward_values : float -- first iteration estimates
+        alpha : float > 0 -- step-size parameter
+        stationarity : string ('stat', 'non_stat') -- determine problem type
+
+        Call setRandomNoise if problem is non-stationary to set gaussian noise
+        '''
+
+        self.alpha = alpha
+        self.rewardsMat = [[] for i in range(arms)]
+        Karmed.__init__(self, arms, mus, sigmas, initial_reward_values)
 
                         
 
